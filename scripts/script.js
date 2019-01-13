fefef4e5fcbd6cf201bcfeca699c00ef5bff57ab
rodolfo <rodolfomayora96@gmail.com>
2019-01-13 12:29:54 -0400

    doc: documentos complementarios

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+let visible = false;
+
+function iniciar() {
+	var elemento = document.getElementById("menu-img");
+	elemento.addEventListener("click", mostrarMenu);
+}
+
+function mostrarMenu() {
+	let elemento = document.getElementById("menuprincipal");
+
+	if (!visible) {
+		elemento.style.display = "block";
+		visible = true;
+	} else {
+		elemento.style.display = "none";
+		visible = false;
+	}
+}
+
+window.addEventListener("load", iniciar);
